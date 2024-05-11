2803c52b2f651ae3e360d4bccab537d38ecb545a
Gugustinette <mercier.augustin@outlook.fr>
2024-05-11 21:08:29 +0200

    Fix Coverage CI

--- docs/vitest.config.ts
+++ docs/vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     reporters: ['html'],
+    coverage: {
+      enabled: true,
+      include: ['packages'],
+      reporter: [['html', { subdir: 'coverage' }]],
+      reportsDirectory: 'fibbo',
+    },
   },
 })
