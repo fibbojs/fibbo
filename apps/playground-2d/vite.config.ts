30a34433004f4fea85a8834003ea36e2f5c07ceb
Gugustinette <mercier.augustin@outlook.fr>
2024-07-06 21:12:37 +0200

    Disable TreeShaking in playground 2D

--- apps/playground-2d/vite.config.ts
+++ apps/playground-2d/vite.config.ts
@@ -8,4 +8,12 @@ export default defineConfig({
     topLevelAwait(),
   ],
   base: '/fibbo/playground-2d/',
+  build: {
+    rollupOptions: {
+      // Solution found here: https://github.com/dimforge/rapier.js/issues/278
+      // Without this option, treeshaking seems to ditch required code from Rapier
+      // Basically results in : "TypeError: Cannot read properties of undefined (reading 'rawintegrationparameters_new')"
+      treeshake: false,
+    },
+  },
 })
