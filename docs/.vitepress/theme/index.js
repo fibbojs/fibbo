e53e26203df5ccf76a24f20c978803c78fa60981
Gugustinette <mercier.augustin@outlook.fr>
2024-11-05 10:27:50 +0100

    docs: press F

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,24 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+// Using https://esm.sh/ to import the ESM module
+import confetti from 'https://esm.sh/canvas-confetti@1.6.0'
+
+/**
+ * Fire confetti when pressing the "F" key
+ * https://confetti.js.org/more.html
+ */
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min
+}
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'f' || e.key === 'F') {
+    confetti({
+      angle: randomInRange(55, 125),
+      spread: randomInRange(50, 70),
+      particleCount: randomInRange(50, 100),
+      origin: { y: 0.6 },
+    })
+  }
+})
 
 export default DefaultTheme
